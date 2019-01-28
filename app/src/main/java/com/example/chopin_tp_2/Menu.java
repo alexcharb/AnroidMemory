5ee5268919c9ec3f85d3d050cc1ce611157f142d
Alex Charbonnier <alexcharb@gmail.com>
2019-01-28 22:14:45 +0100

    intents play & countdown - menu view

--- app/src/main/java/com/example/chopin_tp_2/Menu.java
+++ app/src/main/java/com/example/chopin_tp_2/Menu.java
@@ -1,13 +1,43 @@
 package com.example.chopin_tp_2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
+
+    private Button play = null;
+    private Button countdown = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        play = (Button)findViewById(R.id.playbutton);
+        countdown = (Button)findViewById(R.id.countdownbutton);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,MainActivity.class);
+                intent.putExtra("VALUE", "true");
+                startActivity(intent);
+            }
+        });
+
+        countdown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Menu.this,MainActivity.class);
+                intent.putExtra("VALUE", "false");
+                startActivity(intent);
+            }
+        });
     }
 }
