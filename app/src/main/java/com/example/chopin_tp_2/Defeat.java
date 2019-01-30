1b579084f022523e9914ec73bdd84ad5d536a197
Jeremy-Chopin <36235039+Jeremy-Chopin@users.noreply.github.com>
2019-01-30 20:14:06 +0100

    Add victory and defeat activity

---
+++ app/src/main/java/com/example/chopin_tp_2/Defeat.java
@@ -0,0 +1,13 @@
+package com.example.chopin_tp_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Defeat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defeat);
+    }
+}
